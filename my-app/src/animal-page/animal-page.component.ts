81c8e72002c483a3fc1b749dfa7c5d1542b0f862
Roberta <roberta.rfcosta@gmail.com>
2024-04-24 23:45:01 -0300

    testando projeto

---
+++ my-app/src/animal-page/animal-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animal-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './animal-page.component.html',
+  styleUrl: './animal-page.component.css'
+})
+export class AnimalPageComponent {
+
+}
